c37417dc2633d0fe037d6e90761c6f4b57d26e7a
venkatesh <venkateshlc07@gmail.com>
2024-10-05 10:31:33 +0530

    init

--- src/main/java/com/store/GroceryStore/GroceryStoreApplication.java
+++ src/main/java/com/store/GroceryStore/GroceryStoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GroceryStoreApplication {
-
+   //main
 	public static void main(String[] args) {
 		Receipt r = new Receipt(new ConsolePrinter());
 		r.add("Cookies", new Money(5, "$"));
